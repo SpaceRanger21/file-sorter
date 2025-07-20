c300b72d75214350bbd771d3a5093ab5bf21a75f
Shantanu Dubey <spaceranger2111@gmail.com>
2025-07-21 03:44:11 +0530

    added comments

--- file_sorter.py
+++ file_sorter.py
@@ -1,8 +1,59 @@
+"""
+Description:
+************
+
+File Sorter (https://github.com/SpaceRanger21/file-sorter/blob/master/file_sorter.py) is a Python script used to move
+all files with the same extension in a given directory to a target directory.
+
+This script can be used for organisation as well as specific tasks which needs specific file types from hundreds of
+different file types
+
+How to use:
+***********
+
+To use this script, you have to have Python installed on your computer. You can look the specific instructions to
+install python on your preferred operating system. This script is platform independent meaning it can run on Windows,
+Linux or MacOS.
+
+Open your terminal and run python3 file_sorter.py
+You can also use the Python aliases such as py or python instead of python3
+Follow the on-screen instructions to use the script.
+
+You have to enter the source directory i.e. the directory containing the files,
+                  the destination directory i.e. the directory where the files will be copied or moved
+                  and the extension of the file which is to be copied, such as .txt or .png etc.
+
+The file extension can be typed with or without the dot/period <.>, i.e. you can either type .txt or just txt when
+asked for the input.
+
+The script can move or copy the files. Enter the appropriate number for the required task when prompted to do so.
+
+NOTES:
+******
+
+Throughout the script you will be prompted to make a choice, such as "whether you want to continue or quit" or "whether
+you want to move or copy the files". You move on with the choice, you have to type the number written in front of the
+choice (like 1. Move, 2. Copy) and press enter.
+
+TIP for fellow programmers:
+In the code, there are various print statements with code like "{BackgroundColors.HEADER} " and
+"{BackgroundColors.ENDC}", these are just used to give color to the text on the terminal output. These maybe make the
+code harder to read and look terrifying, but the script will remain just fine without these.
+
+Credits:
+********
+
+Made by:
+SpaceRanger21
+GitHub: https://github.com/SpaceRanger21/
+
+"""
+
 import shutil
 import os
 import time
 
-
+# BackgroundColor class is used to show colorful text in the terminal output.
 class BackgroundColors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -40,7 +91,7 @@ while True:
             moveORcopy = ''
 
             while moveORcopy == '':
-                moveORcopy = input(F'\n1. move\n2. copy\n{BackgroundColors.HEADER}\nWhat do you want to do: {BackgroundColors.ENDC}')
+                moveORcopy = input(F'\n1. Move the files\n2. Copy the files\n{BackgroundColors.HEADER}\nWhat do you want to do: {BackgroundColors.ENDC}')
                 x = moveORcopy.strip()
                 if x.isnumeric() and (x == '1' or x == '2'):
                     break
@@ -110,3 +161,6 @@ while True:
             print(f'{BackgroundColors.FAIL}ERROR: Please enter either 1 or 2{BackgroundColors.ENDC}')
     else:
         print(f'{BackgroundColors.FAIL}ERROR: Please enter either 1 or 2{BackgroundColors.ENDC}')
+
+# Made by SpaceRanger21
+# GitHub: https://github.com/SpaceRanger21/
